fdd123255a5291e1aaf5e927b8050dadda6792f2
masad40 <tasnifmasad40@gmail.com>
2025-12-08 11:09:44 +0600

    compelete navbar

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -8,8 +8,7 @@ const Profile = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="hero min-h-screen bg-gradient-to-br from-gray-100 via-gray-200 to-gray-300 dark:from-gray-900 dark:via-gray-800 dark:to-gray-700 transition-colors duration-500">
-      
+    <div className="hero min-h-screen bg-gradient-to-br from-gray-100 via-gray-200 to-gray-300 dark:from-gray-900 dark:via-gray-800 dark:to-gray-700 transition-colors duration-500">     
       <Helmet>
         <title>My Profile - ImportWave</title>
         <meta
@@ -17,7 +16,6 @@ const Profile = () => {
           content="View and manage your GameHub profile, including account information and avatar."
         />
       </Helmet>
-
       <div className="card w-full max-w-md shadow-2xl bg-white dark:bg-gray-800 rounded-lg transition-colors duration-500">
         <div className="card-body px-8 py-10">
           <h2 className="text-2xl font-bold text-center mb-2 text-gray-900 dark:text-gray-100">
@@ -26,7 +24,6 @@ const Profile = () => {
           <p className="text-center text-gray-600 dark:text-gray-300 mb-6">
             Manage your account information
           </p>
-
           <div className="text-center mb-6">
             <img
               src={
@@ -41,7 +38,6 @@ const Profile = () => {
             </h3>
             <p className="text-gray-600 dark:text-gray-300">{user?.email}</p>
           </div>
-
           <button
             onClick={() => navigate("/updateProfile")}
             className="btn btn-primary w-full bg-blue-600 hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 transition-colors duration-300 text-white"
